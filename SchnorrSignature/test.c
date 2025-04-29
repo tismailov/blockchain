181820542273c05885b87f6bf07337ad16571da5
AlexandrKhromov2005 <alalkhromov@edu.hse.ru>
2025-04-29 11:12:35 +0300

    make test more obvious

--- SchnorrSignature/test.c
+++ SchnorrSignature/test.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <time.h>
+#include <gmp.h>
 #include "signature.h"
 
 int main() {
@@ -21,19 +24,42 @@ int main() {
 
     // Generate keys
     generate_keys(x, P, g, p, state);
+    
+    printf("\n=== Key Generation ===\n");
+    printf("Prime p: ");
+    mpz_out_str(stdout, 16, p);
+    printf("\nGenerator g: ");
+    mpz_out_str(stdout, 16, g);
+    printf("\nPrivate key x: ");
+    mpz_out_str(stdout, 16, x);
+    printf("\nPublic key P: ");
+    mpz_out_str(stdout, 16, P);
+    printf("\n\n");
 
     // Message to sign
     const char *message = "This is a test message for Schnorr signature.";
+    printf("=== Signing Process ===\n");
+    printf("Message to sign: %s\n", message);
 
     // Sign the message
     struct stribog_ctx_t ctx;
     sign_message(message, r, s, x, g, p, state, &ctx);
+    
+    printf("Signature components:\n");
+    printf("r: ");
+    mpz_out_str(stdout, 16, r);
+    printf("\ns: ");
+    mpz_out_str(stdout, 16, s);
+    printf("\n\n");
 
     // Verify the signature
+    printf("=== Verification Process ===\n");
     if (verify_signature(message, r, s, P, g, p, &ctx)) {
-        printf("Signature is valid.\n");
+        printf("Signature verification SUCCESSFUL\n");
+        printf("All components match and the signature is valid!\n");
     } else {
-        printf("Signature is invalid.\n");
+        printf("Signature verification FAILED\n");
+        printf("The signature is invalid or has been tampered with.\n");
     }
 
     gmp_randclear(state);
